e249fb636ef37caaf3c56a76a8ebd753bddce291
Edwin ED <edwin.estradafreelance@gmail.com>
2023-03-31 10:59:33 -0600

    avance3

--- Proyecto_carrito/productos.js
+++ Proyecto_carrito/productos.js
@@ -1,4 +1,4 @@
-let productos=[
+var productos=[
     { 
         id:1,
         img:"img/camisa1.png",
@@ -65,34 +65,30 @@ let productos=[
     },
 ]
 let html="";
-let contador=0;
+let contador=1;
 productos.forEach(product=>{
-    html=html+`<div class="producto"> <img src="${product.img}"/><div class="precio"><p>${product.nombre} </p><p id="precio" name="otra">${product.precio}</p></div><p>${product.descripcion}</p><div class="radio"><input type="radio"  name="talla${product.id}"><label>S</label><input type="radio" name="talla${product.id}"><label>M</label><input type="radio"  name="talla${product.id}" ><label>L</label> </div><button class="${product.id}"type="button" id="id${contador}" >Agregar al carrito <i class="bi bi-cart4"></i></button> </div>`;
+    html=html+`<div class="producto"> <img src="${product.img}"/><div class="precio"><p>${product.nombre} </p><p id="precio" name="otra">${product.precio}</p></div><p>${product.descripcion}</p><div class="radio"><input type="radio"  name="talla${product.id}"><label>S</label><input type="radio" name="talla${product.id}"><label>M</label><input type="radio"  name="talla${product.id}" ><label>L</label> </div><button "type="button" id="id${product.id}">Agregar al carrito <i class="bi bi-cart4"></i></button> </div>`;
     contador++;
 });
 
-document.getElementById("container").innerHTML=html;
-var carrito=[];
-let evento=document.querySelector("#id1")
-evento.addEventListener("click", add);
-function add(){
-    let car;
-    car=productos.filter(product=>product.id===Number(evento.className));
-    carrito=[...car];
+let con=document.getElementById("container");
+con.insertAdjacentHTML("afterbegin",html)
 
+const carrito=[];
+let car;
+document.getElementById("id1").onclick = function(){
+    car=productos.filter(product=>product.id===1);
+    carrito.push([...car]);
+    document.getElementById("cart1").innerHTML=carrito.length;
 }
-let evento2=document.querySelector("#id2")
-evento2.addEventListener("Click", add);
-function add(){
-    let car;
-    car=productos.filter(product=>product.id===Number(evento.className));
-    carrito=[...car];
+document.getElementById("id2").onclick = function(){
+    car=productos.filter(product=>product.id===2);
+    carrito.push([...car]);
+    document.getElementById("cart1").innerHTML=carrito;
+}
 
-}
-/*let prueba=productos.filter(product=>product.id===2);
-let nuevo=[...prueba];
-console.log(nuevo[0].id)*/
 document.getElementById("cart1").innerHTML=carrito.length;
+
 
 
 
